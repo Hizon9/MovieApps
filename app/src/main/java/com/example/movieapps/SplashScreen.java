df4c2cfaec4301364d31197bb9b660f8db8687bd
HizOn <hizkiaono@gmail.com>
2020-07-30 22:37:11 +0700

    Initial commit

---
+++ app/src/main/java/com/example/movieapps/SplashScreen.java
@@ -0,0 +1,26 @@
+package com.example.movieapps;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 4000;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent splash = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(splash);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
