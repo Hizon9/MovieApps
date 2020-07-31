746df1beb72c13b4bfab3798b147ce09a03d8a61
HizOn <hizkiaono@gmail.com>
2020-07-31 18:24:51 +0700

    fetch data alpha

--- app/src/main/java/com/example/movieapps/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/movieapps/ui/dashboard/DashboardFragment.java
@@ -12,24 +12,29 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.movieapps.FetchDataSeries;
 import com.example.movieapps.R;
 
 public class DashboardFragment extends Fragment {
 
     private DashboardViewModel dashboardViewModel;
+    public static TextView data;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         dashboardViewModel =
                 ViewModelProviders.of(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        data = (TextView) root.findViewById(R.id.text_dashboard);
+        FetchDataSeries process = new FetchDataSeries();
+        process.execute();
+        //final TextView textView = root.findViewById(R.id.text_dashboard);
+        //.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            //@Override
+            //public void onChanged(@Nullable String s) {
+                //textView.setText(s);
+            //}
+        //});
         return root;
     }
 }
